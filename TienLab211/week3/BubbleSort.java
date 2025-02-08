5e2ccc3693fa37f4293b515fd3b2b93d8c2b949c
Tien-nu <nguyenthithuytiensmstan@gmail.com>
2025-02-08 13:50:31 +0700

    Add files via upload

---
+++ TienLab211/week3/BubbleSort.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TienLab211.week3;
+
+import java.util.Random;
+
+/**
+ *
+ * @author nguyenthithuytien
+ */
+public class BubbleSort {
+
+    private int[] arr;
+    
+    public BubbleSort(int n) {
+        arr = new int[n];
+        addValue();
+    }
+    
+    void addValue() {
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = rand.nextInt(arr.length);
+        }
+    }
+    
+    void Sort(){
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = 0; j < arr.length - i - 1; j++){
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp; 
+                }
+            }
+        }
+    }
+    
+    void display(){
+        for (int i: arr){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ TienLab211/week3/Test.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TienLab211.week3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nguyenthithuytien
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Bubble Sort");
+            System.out.println("2. Quick Sort");
+            System.out.println("3. Binary Search");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+            
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    int n1 = GetInput.getInt("Enter number of array");
+                    BubbleSort bubble = new BubbleSort(n1);
+                    System.out.println("Unsorted array:");
+                    bubble.display();
+                    bubble.Sort();
+                    System.out.println("Sorted array:");
+                    bubble.display();
+                    break;
+                case 2:
+                    int n2 = GetInput.getInt("Enter number of array");
+                    QuickSort quick = new QuickSort(n2);
+                    System.out.println("Unsorted array:");
+                    quick.display();
+                    quick.sort();
+                    System.out.println("Sorted array:");
+                    quick.display();
+                    break;
+                case 3:
+                    int n3 = GetInput.getInt("Enter number of array");
+                    int value = GetInput.getInt("Enter search value");
+                    BinarySearch search = new BinarySearch(n3);
+                    search.display();
+                    int result = search.binarySearch(value);
+                    if (result == -1) System.out.println("Not found");
+                    else System.out.println("Found " + value + " at index: " + result);
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+            }
+        }
+    }
+}
